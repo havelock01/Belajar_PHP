0289ce6bbc3dbbaea991e2ac663d2ffc764a99b9
Muhammad Rizqi <mrizqi0153@gmail.com>
2023-11-01 05:58:01 +0700

    Done sintak dasar

--- index.php
+++ index.php
@@ -43,17 +43,41 @@
     $x = 10;
     $y = 20;
     echo $x*$y;
-    echo "<br>";
+    echo "<br><br><br>";
 
     // Operator penggabung string (concatenation/concat) operatornya itu yaitu titik (.)
 
     $jurusan = "Informatika";
     $fakultas = "Teknik Industri";
-    echo "Jurusan ". $jurusan .", Fakultas ". $fakultas;
+    echo "Jurusan ". $jurusan .", Fakultas ". $fakultas . "<br>";
 
     // Assignment => =, +=, -=, *=, /=, %=, .=
     $number = 5;
     $number += 2;
-    echo $number;
+    echo "<br>". $number. "<br>";
 
+    $nama2 = "Muhammad";
+    $nama2 .= " ";
+    $nama2 .= "Rizqi";
+    echo $nama2. "<br><br>";
+    
+    // Perbandingan => <, >, <=, >=, ==, !=
+    var_dump(1<5);
+    echo "<br>";
+    echo "<br>";
+    // tapi kalau misal gini :
+    var_dump(1=="1");
+    echo "<br>";
+    echo "<br>";
+    // dia akan true karena di var_dump hanya ngecek nilainya bukan tipe datanya
+
+    //untuk bisa ngecek identitasnya ada caranya :
+    // Dengan "Identitas" => ===, !==
+    var_dump(1==="1");
+    echo "<br>";
+    echo "<br>";
+
+    // Operator logika => &&, ||, !
+    $z= 10;
+    var_dump($z<20 && $x%2==0);
 ?>
