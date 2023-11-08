2650fa2a2adaa5bdc26067e0b85507dad16d11ff
Muhammad Rizqi <mrizqi0153@gmail.com>
2023-11-08 20:10:58 +0700

    belajar pengulangan

--- index.php
+++ index.php
@@ -1,3 +1,26 @@
-<?php 
-echo "First Initial";
+<?php
+// *******Pengulangan********
+
+// for
+echo "For Conditional : <br>";
+for ($i = 0; $i < 5; $i++) {
+    echo "Hello world! <br>";
+}
+
+// while
+echo "While Conditonal : <br>";
+$a = 0;
+while ($a < 5) {
+    echo "Hello world! <br>";
+    $a++;
+}
+
+// do..while
+echo "do...while Condition : <br>";
+$b = 0;
+do {
+    echo "Hello world! <br>";
+} while ($b < 5);
+
+// foreach : pengulangan khusus array
 ?>
