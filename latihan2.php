00f0c8a743fa40b0bd77d59e2aef494e90cf17a4
Muhammad Rizqi <mrizqi0153@gmail.com>
2023-11-08 21:47:05 +0700

    done sampai if else

---
+++ latihan2.php
@@ -0,0 +1,24 @@
+<?php
+// Pengkondisian / Percabangan
+
+// if else
+$x = 20;
+if ($x < 20) {
+    echo "<br>benar";
+} else {
+    echo "<br>salah";
+}
+
+// if else if else
+$y = 30;
+if ($y < 20) {
+    echo "<br>Benar";
+} else if ($y == 20) {
+    echo "<br>Bingo!";
+} else {
+    echo "<br>salah";
+}
+
+// ternary
+// switch
+?>
