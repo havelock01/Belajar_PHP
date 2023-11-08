4ce83c2f95e6084dcf9f973b2dd221f0194df14c
Muhammad Rizqi <mrizqi0153@gmail.com>
2023-11-08 20:25:32 +0700

    bikin table dengan pengulangan cara pertama

--- latihan1.php
+++ latihan1.php
@@ -9,20 +9,15 @@
 
 <body>
     <table border="1" cellpadding="10" cellspacing="0">
-        <tr>
-            <td>1,1</td>
-            <td>1,2</td>
-            <td>1,3</td>
-            <td>1,4</td>
-            <td>1,5</td>
-        </tr>
-        <tr>
-            <td>2,1</td>
-            <td>2,2</td>
-            <td>2,3</td>
-            <td>2,4</td>
-            <td>2,5</td>
-        </tr>
+        <?php
+        for ($i = 1; $i <= 3; $i++) {
+            echo "<tr>";
+            for ($j = 1; $j <= 5; $j++) {
+                echo "<td>$i,$j</td>";
+            }
+            echo "</tr>";
+        }
+        ?>
     </table>
 </body>
 
